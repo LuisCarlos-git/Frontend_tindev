8bcbbb1b4f1295d2b68a61a95b09833475af38da
luis carlos <luisestrela.bol951@gmail.com>
2020-01-03 17:37:20 -0300

    main page complete

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import api from '../../services/api';
 import './login.css'
 
 import logo from '../../assets/logo.svg';
 
-export default function Login(){
+export default function Login({ history }){
+    const [ username, setUsername ]= useState('');
+
+    async function handleSubmit(event){
+        event.preventDefault();
+        const response = await api.post('/devs', { username })
+
+        const { _id } = response.data;
+
+        history.push(`/dev/${_id}`)
+    };
+
+    
+
     return (
         <div className="container">           
 
-            <form>
+            <form onSubmit={handleSubmit}>
             <img src={logo} alt="TinDev" title="Tindev"/>
             <label htmlFor="usergit">Seu usuario no github *</label>
             <input 
                 placeholder="Digite seu usuario no github"
                 type="text"
                 id="usergit"
+                value={username}
+                onChange={event => setUsername(event.target.value)}
             />
-
             <button type="submit">Entrar</button>
         </form>
     </div>
--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,9 +1,70 @@
 import React from 'react';
 
+import logo from '../../assets/logo.svg';
+import like from '../../assets/like.svg';
+import dislike from '../../assets/dislike.svg';
 
+import './main.css'
 
-export default function Main() {
+export default function Main({ match }) {
     return(
-        <h1>hello world</h1>
+        <div className="container-main">
+            <img src={logo} alt=""/>
+            <ul>
+                <li>
+                    <img src="https://avatars3.githubusercontent.com/u/53982648?v=4" alt=""/>
+                    <footer>
+                        <strong>luis carlos</strong>
+                        <p>Programador durante todo o dia</p>
+                    </footer>
+
+                    <div className="button">
+                        <button type="button">
+                            <img src={like} alt=""/>
+                        </button>
+
+                        <button type="button">
+                            <img src={dislike} alt=""/>
+                        </button>
+                    </div>
+                </li>
+
+                <li>
+                    <img src="https://avatars3.githubusercontent.com/u/53982648?v=4" alt=""/>
+                    <footer>
+                        <strong>luis carlos</strong>
+                        <p>Programador durante todo o dia</p>
+                    </footer>
+
+                    <div className="button">
+                        <button type="button">
+                            <img src={like} alt=""/>
+                        </button>
+
+                        <button type="button">
+                            <img src={dislike} alt=""/>
+                        </button>
+                    </div>
+                </li>
+
+                <li>
+                    <img src="https://avatars3.githubusercontent.com/u/53982648?v=4" alt=""/>
+                    <footer>
+                        <strong>luis carlos</strong>
+                        <p>Programador durante todo o dia</p>
+                    </footer>
+
+                    <div className="button">
+                        <button type="button">
+                            <img src={like} alt=""/>
+                        </button>
+
+                        <button type="button">
+                            <img src={dislike} alt=""/>
+                        </button>
+                    </div>
+                </li>
+            </ul>
+        </div>
     );
 }
